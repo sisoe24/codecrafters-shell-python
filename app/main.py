ff387e9980e921e1d61dabbc43606d5cf0f97625
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2024-12-14 18:13:22 -0500

    The echo builtin

--- app/main.py
+++ app/main.py
@@ -1,4 +1,17 @@
 import sys
+from dataclasses import dataclass
+
+
+@dataclass
+class Command:
+    bin: str
+    args: list[str]
+
+
+
+def parseInput(text: str) -> Command:
+    parts = text.split(' ')
+    return Command(bin=parts[0], args=parts[1:])
 
 
 def main():
@@ -7,10 +20,14 @@ def main():
 
     while True:
         result = input()
+        command = parseInput(result)
+        str_args = ' '.join(command.args)
 
-        match result:
-            case "exit 0":
+        match command.bin:
+            case "exit":
                 sys.exit(0)
+            case "echo":
+                print(str_args)
             case _:
                 print(f'{result}: command not found')
 
