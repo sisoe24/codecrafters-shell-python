de0a8d8cd3ae4c5d4628c1d144c402516e9590c7
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2024-12-14 18:07:24 -0500

    REPL

--- app/main.py
+++ app/main.py
@@ -2,12 +2,13 @@ import sys
 
 
 def main():
-    # Uncomment this block to pass the first stage
+
     sys.stdout.write("$ ")
 
-    # Wait for user input
-    result = input()
-    print(f'{result}: command not found')
+    while True:
+        result = input()
+        print(f'{result}: command not found')
+        sys.stdout.write("$ ")
 
 
 if __name__ == "__main__":
